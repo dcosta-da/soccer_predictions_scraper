7a3fb85c75a394dfd18e710474d769e7046090f2
Asabuzz <damien.cds@gmail.com>
2024-12-13 17:40:51 +0100

    change python version to 3.11 for workflow

--- excel_export.py
+++ excel_export.py
@@ -3,9 +3,16 @@ import pandas as pd
 
 def export_to_excel(df):
     file_path = "betclever_predictions.xlsx"
+
     sheets = {
-        "Home win": df[(df['Home Win (%)'] >= 70) & (df['Home Odds'] >= 1.7)],
-        # Ajouter d'autres filtres pour chaque feuille de ton Excel
+    "Home win": df[(df['Home Win (%)'] >= 70) & (df['Home Odds'] >= 1.7)][['Date', 'Country', 'Championship', 'Match', 'Home Win (%)', 'Home Odds']],
+    "Away Win": df[(df['Away Win (%)'] >= 70) & (df['Away Odds'] >= 1.7)][['Date', 'Country', 'Championship', 'Match', 'Away Win (%)', 'Away Odds']],
+    "Draw": df[df['Draw (%)'] >= 60][['Date', 'Country', 'Championship', 'Match', 'Draw (%)']],
+    "Btts": df[(df['Btts (%)'] >= 75) & (df['Odds btts'] >= 1.7)][['Date', 'Country', 'Championship', 'Match', 'Btts (%)', 'Odds btts']],
+    "Over_Under": df[
+        ((df['Over 2.5 (%)'] >= 80) & (df['Odds 2.5'] >= 1.7)) |
+        ((df['Over 3.5 (%)'] >= 60) & (df['Odds 3.5'] >= 2.2))
+    ][['Date', 'Country', 'Championship', 'Match', 'Over 2.5 (%)', 'Odds 2.5', 'Over 3.5 (%)', 'Odds 3.5']]
     }
     
     # Si le fichier existe déjà, on l'ouvre en mode ajout
