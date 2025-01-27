c81e1beb4252ba999a9423fc2fcfb12102b3a6e0
Asabuzz <damien.cds@gmail.com>
2025-01-27 17:13:23 +0100

    refacto structure folder

--- scraper/main.py
+++ scraper/main.py
@@ -1,9 +1,7 @@
-import time
-import os
-from selenium_config import configure_driver
-from scraper import scrape_data
-from data_cleaning import clean_data, process_data
-from excel_export import export_to_excel
+from functions.selenium_config import configure_driver
+from functions.scraper import scrape_data
+from functions.data_cleaning import clean_data, process_data
+from functions.excel_export import export_to_excel
 
 def main():
     # Configurer Selenium
