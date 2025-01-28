7f17f97efc2390458bdf59307b54508670ec5ad3
dcosta-da <damien.cds@gmail.com>
2025-01-28 18:45:22 +0100

    change title pages

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import streamlit as st
 
-home_page = st.Page("home.py", title="Betclever scraper", icon="🏠")
+home_page = st.Page("home.py", title="Home", icon="🏠")
 predictions_page = st.Page("predictions.py", title="Predictions", icon="🎯")
 
 pg = st.navigation([home_page, predictions_page])
-st.set_page_config(page_title="Betclever predictions", page_icon="⚽", layout="wide")
+st.set_page_config(page_title="Golden goal", page_icon="⚽", layout="wide")
 pg.run()
 
 
