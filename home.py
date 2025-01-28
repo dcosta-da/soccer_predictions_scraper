148e4e23b7d58619029133ce45085553e11306b0
dcosta-da <damien.cds@gmail.com>
2025-01-28 18:53:52 +0100

    test text and markdown

--- home.py
+++ home.py
@@ -1,3 +1,12 @@
 import streamlit as st
 
-st.title('Betclever scraper')
+st.title("Home")
+
+st.header("What's Golden Goal ?", divider=True)
+
+st.header("What are the predictions available ?", divider=True)
+st.text("Some text")
+st.markdown("""Some text
+            - with list
+            - item 2""")
+st.text("Some text")
