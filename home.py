8c4a5ea33c221b32a15736df647cd41640036895
Asabuzz <damien.cds@gmail.com>
2025-01-29 14:13:11 +0100

    presentation

--- home.py
+++ home.py
@@ -5,8 +5,27 @@ st.title("Home")
 # Part 1
 st.header("What's Golden Goal ?", divider="blue")
 
+# st.markdown(
+# """
+# Golden Goal is a streamlit application that displays some of the predictions offered on [Betclever](betclever.com) website 
+# on different football match outcomes. 
+
+# It was created for personal betting strategies and share an use case on how to optimise 
+# the data shared on Betclever. 
+
+# Betclever is a serious site that offers free daily predictions on a very large number of football championships. 
+
+# This streamlit app is in no way a replacement site, so do not hesitate to visit the site daily and follow it on [Facebook](https://www.facebook.com/BCbetclever), 
+# [Instagram](https://www.instagram.com/betclever_/), [X](https://x.com/betclever_) and [Telegram](https://t.me/+VrHDodda6ssyOGZk).
+
+# ⚠️ **Disclaimer**
+# - This is not an incentive to bet : this streamlit application will in no way be held responsible for any potential financial losses linked to betting. 
+# """)
+
 # Part 2
-st.header("What are the data available ?", divider="blue")
+st.header("What predictions are displayed ?", divider="blue")
+
+st.subheader('Home & Away win')
 
 st.text("Some text")
 st.markdown("""
@@ -14,3 +33,7 @@ st.markdown("""
             - item 2
             """)
 st.write("Some text")
+
+st.subheader("Both teams to score")
+
+st.subheader("Over goals")
