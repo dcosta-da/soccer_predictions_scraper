285f24228b497de1ee64c3bceeb9c6a43f492b28
dcosta-da <damien.cds@gmail.com>
2025-02-16 21:19:05 +0100

    adding date filter

--- functions/data_loader.py
+++ functions/data_loader.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import matplotlib 
 import pandas as pd
+from datetime import datetime, date
 
 file_path = "scraper/data/betclever_predictions.xlsx"
 
@@ -8,27 +9,36 @@ file_path = "scraper/data/betclever_predictions.xlsx"
 def display_proba_dataframe(sheet_name, prob_col, title):
     st.header(title, divider="red")
     
-    col1, col2 = st.columns(2)
+    col1, col2, col3 = st.columns(3)
     
     df = pd.read_excel(file_path, sheet_name=sheet_name)
     
+    df['Date'] = pd.to_datetime(df['Date'])
+
+    min_date, max_date = df['Date'].min().date(), df['Date'].max().date()
     championships_selection = sorted((df["Country"] + " - " + df["Championship"]).unique())
     min_proba, max_proba = df[prob_col].min(), df[prob_col].max()
     
     with col1:
-        selected_championships = st.multiselect("Choose one or many championship(s)", championships_selection, placeholder="Championships available")
+        selected_date = st.date_input("Choose a date", value=date.today(), format="YYYY-MM-DD", min_value=min_date, max_value=max_date, key=f"date_input_{sheet_name}")
     
     with col2:
-        selected_proba = st.slider('Choose the minimum probability', min_value=min_proba, max_value=max_proba)
+        selected_championships = st.multiselect("Choose one or many championship(s)", championships_selection, placeholder="Championships available")
+
+    with col3:
+        selected_proba = st.slider("Choose the minimum probability", min_value=min_proba, max_value=max_proba)
     
     st.write("")
-    
+
+    if selected_date:
+        df = df[df['Date'].dt.date == selected_date]  
+
     if selected_championships:
         df = df[(df["Country"] + " - " + df["Championship"]).isin(selected_championships)]
     
     if selected_proba != min_proba:
         df = df[df[prob_col] >= selected_proba]
-    
+
     st.dataframe(df.style.background_gradient(subset=[prob_col], cmap='Reds'))
 
 
@@ -36,20 +46,29 @@ def display_ev_dataframe(sheet_name, ev_filter_col, ev_value, title):
     st.header(title, divider="red")
 
     df = pd.read_excel(file_path, sheet_name=sheet_name)
+    df['Date'] = pd.to_datetime(df['Date'])
+
     df_filtered = df[df[ev_filter_col] >= ev_value]
 
-    col1, col2 = st.columns(2)
+    col1, col2, col3 = st.columns(3)
 
+    min_date, max_date = df['Date'].min().date(), df['Date'].max().date()
     championships_selection = sorted((df_filtered["Country"] + " - " + df_filtered["Championship"]).unique())
     min_ev, max_ev = df_filtered[ev_filter_col].min(), df_filtered[ev_filter_col].max()
 
     with col1:
+        selected_date = st.date_input("Choose a date", value=date.today(), format="YYYY-MM-DD", min_value=min_date, max_value=max_date, key=f"date_input_{sheet_name}")
+    
+    with col2:
         selected_championships = st.multiselect("Choose one or many championship(s)", championships_selection, placeholder="Championships available")
 
-    with col2:
+    with col3:
         selected_proba = st.slider('Choose the minimum EV', min_value=min_ev, max_value=max_ev)
 
     st.write("")
+
+    if selected_date:
+        df = df[df['Date'].dt.date == selected_date]  
 
     if selected_championships:
         df_filtered = df_filtered[(df_filtered["Country"] + " - " + df_filtered["Championship"]).isin(selected_championships)]
