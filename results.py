707050319b7c844368d39220cb11dcbdc87146de
dcosta-da <damien.cds@gmail.com>
2025-02-17 21:50:44 +0100

    adding date filter

--- results.py
+++ results.py
@@ -2,6 +2,6 @@ import streamlit as st
 
 st.set_page_config(page_title="Golden goal", page_icon="⚽", layout="wide")
 
-st.title("💶 Profit/loss")
+st.title("🫰 Profit/loss")
 
 st.header("Strategy betting & results", divider="red")
