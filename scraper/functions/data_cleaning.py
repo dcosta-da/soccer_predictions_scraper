8b07675372a2a674118751e053ff2e322f8ac3c8
dcosta-da <damien.cds@gmail.com>
2025-04-06 19:52:48 +0200

    fix bug df columns

--- scraper/functions/data_cleaning.py
+++ scraper/functions/data_cleaning.py
@@ -104,17 +104,37 @@ def process_data(df):
     
 #     return df
 
+# def compute_ev(df):
+#     if 'Home Win (%)' in df.columns and 'Home Odds' in df.columns:
+#         df['EV home win'] = round(((df['Home Win (%)'] / 100) * ((df['Home Odds'] - 1))) - ((1 - (df['Home Win (%)'] / 100)) * 1), 2)
+
+#     if 'Away Win (%)' in df.columns and 'Away Odds' in df.columns:
+#         df['EV away win'] = round(((df['Away Win (%)'] / 100) * ((df['Away Odds'] - 1))) - ((1 - (df['Away Win (%)'] / 100)) * 1), 2)
+
+#     if 'Btts (%)' in df.columns and 'Odds btts' in df.columns:
+#         df['EV btts'] = round(((df['Btts (%)'] / 100) * ((df['Odds btts'] - 1))) - ((1 - (df['Btts (%)'] / 100)) * 1), 2)
+
+#     if 'Over 2.5 (%)' in df.columns and 'Odds 2.5' in df.columns:
+#         df['EV over 2.5'] = round(((df['Over 2.5 (%)'] / 100) * ((df['Odds 2.5'] - 1))) - ((1 - (df['Over 2.5 (%)'] / 100)) * 1), 2)
+
+#     return df
+
 def compute_ev(df):
-    if 'Home Win (%)' in df.columns and 'Home Odds' in df.columns:
-        df['EV home win'] = round(((df['Home Win (%)'] / 100) * ((df['Home Odds'] - 1))) - ((1 - (df['Home Win (%)'] / 100)) * 1), 2)
+    ev_calculations = {
+        'EV home win': ('Home Win (%)', 'Home Odds'),
+        'EV away win': ('Away Win (%)', 'Away Odds'),
+        'EV btts': ('Btts (%)', 'Odds btts'),
+        'EV over 2.5': ('Over 2.5 (%)', 'Odds 2.5')
+    }
 
-    if 'Away Win (%)' in df.columns and 'Away Odds' in df.columns:
-        df['EV away win'] = round(((df['Away Win (%)'] / 100) * ((df['Away Odds'] - 1))) - ((1 - (df['Away Win (%)'] / 100)) * 1), 2)
-
-    if 'Btts (%)' in df.columns and 'Odds btts' in df.columns:
-        df['EV btts'] = round(((df['Btts (%)'] / 100) * ((df['Odds btts'] - 1))) - ((1 - (df['Btts (%)'] / 100)) * 1), 2)
-
-    if 'Over 2.5 (%)' in df.columns and 'Odds 2.5' in df.columns:
-        df['EV over 2.5'] = round(((df['Over 2.5 (%)'] / 100) * ((df['Odds 2.5'] - 1))) - ((1 - (df['Over 2.5 (%)'] / 100)) * 1), 2)
+    for ev_name, (prob_col, odds_col) in ev_calculations.items():
+        if prob_col in df.columns and odds_col in df.columns:
+            df[ev_name] = round(
+                ((df[prob_col] / 100) * (df[odds_col] - 1)) -
+                ((1 - (df[prob_col] / 100)) * 1), 2
+            )
+        else:
+            print(f"Colonnes manquantes pour '{ev_name}': '{prob_col}' ou '{odds_col}'. Remplissage avec NaN.")
+            df[ev_name] = np.nan
 
     return df
